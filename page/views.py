db6a1954ba7b8b422079f1d0bbe086b3508b996d
Ezequiel Pereira Lopez <eze2307@gmail.com>
2014-09-21 20:52:35 -0300

    Info en perfil

--- page/views.py
+++ page/views.py
@@ -37,6 +37,26 @@ from calendar import monthrange
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+def age(day, month, year, cday=0, cmonth=0, cyear=0):
+    date(year, month, day)
+    if cday:
+        today = date(cyear, cmonth, cday)
+    else:
+        today = date.today()
+    years = today.year-year
+    months = today.month-month
+    days = today.day-day
+    if days < 0:
+        months -= 1
+        days += monthrange(year, month)[1]
+    if months < 0:
+        years -= 1
+        months += 12
+    if years < 0:
+        raise ValueError, "the given date wasn't reached yet!"
+    return days, months, years
+
+country_codes = {"AF":"Afghanistan", "AL":"Albania", "DZ":"Algeria", "AS":"American Samoa", "AD":"Andorra", "AG":"Angola", "AI":"Anguilla", "AG":"Antigua & Barbuda", "AR":"Argentina", "AA":"Armenia", "AW":"Aruba", "AU":"Australia", "AT":"Austria", "AZ":"Azerbaijan", "BS":"Bahamas", "BH":"Bahrain", "BD":"Bangladesh", "BB":"Barbados", "BY":"Belarus", "BE":"Belgium", "BZ":"Belize", "BJ":"Benin", "BM":"Bermuda", "BT":"Bhutan", "BO":"Bolivia", "BL":"Bonaire", "BA":"Bosnia & Herzegovina", "BW":"Botswana", "BR":"Brazil", "BC":"British Indian Ocean Ter", "BN":"Brunei", "BG":"Bulgaria", "BF":"Burkina Faso", "BI":"Burundi", "KH":"Cambodia", "CM":"Cameroon", "CA":"Canada", "IC":"Canary Islands", "CV":"Cape Verde", "KY":"Cayman Islands", "CF":"Central African Republic", "TD":"Chad", "CD":"Channel Islands", "CL":"Chile", "CN":"China", "CI":"Christmas Island", "CS":"Cocos Island", "CO":"Colombia", "CC":"Comoros", "CG":"Congo", "CK":"Cook Islands", "CR":"Costa Rica", "CT":"Cote D'Ivoire", "HR":"Croatia", "CU":"Cuba", "CB":"Curacao", "CY":"Cyprus", "CZ":"Czech Republic", "DK":"Denmark", "DJ":"Djibouti", "DM":"Dominica", "DO":"Dominican Republic", "TM":"East Timor", "EC":"Ecuador", "EG":"Egypt", "SV":"El Salvador", "GQ":"Equatorial Guinea", "ER":"Eritrea", "EE":"Estonia", "ET":"Ethiopia", "FA":"Falkland Islands", "FO":"Faroe Islands", "FJ":"Fiji", "FI":"Finland", "FR":"France", "GF":"French Guiana", "PF":"French Polynesia", "FS":"French Southern Ter", "GA":"Gabon", "GM":"Gambia", "GE":"Georgia", "DE":"Germany", "GH":"Ghana", "GI":"Gibraltar", "GB":"Great Britain", "GR":"Greece", "GL":"Greenland", "GD":"Grenada", "GP":"Guadeloupe", "GU":"Guam", "GT":"Guatemala", "GN":"Guinea", "GY":"Guyana", "HT":"Haiti", "HW":"Hawaii", "HN":"Honduras", "HK":"Hong Kong", "HU":"Hungary", "IS":"Iceland", "IN":"India", "ID":"Indonesia", "IA":"Iran", "IQ":"Iraq", "IR":"Ireland", "IM":"Isle of Man", "IL":"Israel", "IT":"Italy", "JM":"Jamaica", "JP":"Japan", "JO":"Jordan", "KZ":"Kazakhstan", "KE":"Kenya", "KI":"Kiribati", "NK":"Korea North", "KS":"Korea South", "KW":"Kuwait", "KG":"Kyrgyzstan", "LA":"Laos", "LV":"Latvia", "LB":"Lebanon", "LS":"Lesotho", "LR":"Liberia", "LY":"Libya", "LI":"Liechtenstein", "LT":"Lithuania", "LU":"Luxembourg", "MO":"Macau", "MK":"Macedonia", "MG":"Madagascar", "MY":"Malaysia", "MW":"Malawi", "MV":"Maldives", "ML":"Mali", "MT":"Malta", "MH":"Marshall Islands", "MQ":"Martinique", "MR":"Mauritania", "MU":"Mauritius", "ME":"Mayotte", "MX":"Mexico", "MI":"Midway Islands", "MD":"Moldova", "MC":"Monaco", "MN":"Mongolia", "MS":"Montserrat", "MA":"Morocco", "MZ":"Mozambique", "MM":"Myanmar", "NA":"Nambia", "NU":"Nauru", "NP":"Nepal", "AN":"Netherland Antilles", "NL":"Netherlands (Holland, Europe)", "NV":"Nevis", "NC":"New Caledonia", "NZ":"New Zealand", "NI":"Nicaragua", "NE":"Niger", "NG":"Nigeria", "NW":"Niue", "NF":"Norfolk Island", "NO":"Norway", "OM":"Oman", "PK":"Pakistan", "PW":"Palau Island", "PS":"Palestine", "PA":"Panama", "PG":"Papua New Guinea", "PY":"Paraguay", "PE":"Peru", "PH":"Philippines", "PO":"Pitcairn Island", "PL":"Poland", "PT":"Portugal", "PR":"Puerto Rico", "QA":"Qatar", "ME":"Republic of Montenegro", "RS":"Republic of Serbia", "RE":"Reunion", "RO":"Romania", "RU":"Russia", "RW":"Rwanda", "NT":"St Barthelemy", "EU":"St Eustatius", "HE":"St Helena", "KN":"St Kitts-Nevis", "LC":"St Lucia", "MB":"St Maarten", "PM":"St Pierre & Miquelon", "VC":"St Vincent & Grenadines", "SP":"Saipan", "SO":"Samoa", "AS":"Samoa American", "SM":"San Marino", "ST":"Sao Tome & Principe", "SA":"Saudi Arabia", "SN":"Senegal", "RS":"Serbia", "SC":"Seychelles", "SL":"Sierra Leone", "SG":"Singapore", "SK":"Slovakia", "SI":"Slovenia", "SB":"Solomon Islands", "OI":"Somalia", "ZA":"South Africa", "ES":"Spain", "LK":"Sri Lanka", "SD":"Sudan", "SR":"Suriname", "SZ":"Swaziland", "SE":"Sweden", "CH":"Switzerland", "SY":"Syria", "TA":"Tahiti", "TW":"Taiwan", "TJ":"Tajikistan", "TZ":"Tanzania", "TH":"Thailand", "TG":"Togo", "TK":"Tokelau", "TO":"Tonga", "TT":"Trinidad & Tobago", "TN":"Tunisia", "TR":"Turkey", "TU":"Turkmenistan", "TC":"Turks & Caicos Is", "TV":"Tuvalu", "UG":"Uganda", "UA":"Ukraine", "AE":"United Arab Emirates", "GB":"United Kingdom", "US":"United States of America", "UY":"Uruguay", "UZ":"Uzbekistan", "VU":"Vanuatu", "VS":"Vatican City State", "VE":"Venezuela", "VN":"Vietnam", "VB":"Virgin Islands (Brit)", "VA":"Virgin Islands (USA)", "WK":"Wake Island", "WF":"Wallis & Futana Is", "YE":"Yemen", "ZR":"Zaire", "ZM":"Zambia", "ZW":"Zimbabwe"}
 
 # Create your views here.
 
@@ -217,6 +237,20 @@ class EditProfileView(LoginRequiredMixin, TemplateView):
     def split_semicolon(self):
         return self.split(";")
 
+    def get(self, request):
+        self.request = request
+        return super(EditProfileView, self).get(request)
+
+    def get_context_data(self, **kwargs):
+        context = super(EditProfileView, self).get_context_data(**kwargs)
+        user = User.objects.get(username=self.request.user)
+        user_age = age(user.date_of_birth.day, user.date_of_birth.month, user.date_of_birth.year)
+        context['days'] = "%d %s" % (user_age[0], ("day" if user_age[0]==1 else "days"))
+        context['months'] = "%d %s" % (user_age[1], ("month" if user_age[1]==1 else "months"))
+        context['years'] = "%d %s" % (user_age[2], ("year" if user_age[2]==1 else "years"))
+        context['country'] = country_codes[user.country]
+        return context
+
 
 class AddLangRequest(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
@@ -263,25 +297,6 @@ class RemoveLangRequest(LoginRequiredMixin, View):
 
 
 class SearchPeopleRequest(LoginRequiredMixin, View):
-    def age(self, day, month, year, cday=0, cmonth=0, cyear=0):
-        date(year, month, day)
-        if cday:
-            today = date(cyear, cmonth, cday)
-        else:
-            today = date.today()
-        years = today.year-year
-        months = today.month-month
-        days = today.day-day
-        if days < 0:
-            months -= 1
-            days += monthrange(year, month)[1]
-        if months < 0:
-            years -= 1
-            months += 12
-        if years < 0:
-            raise ValueError, "the given date wasn't reached yet!"
-        return days, months, years
-
     def get(self, request, *args, **kwargs):
         userGet = User.objects.get(username=request.user)
         users = {}
@@ -295,7 +310,7 @@ class SearchPeopleRequest(LoginRequiredMixin, View):
                     print user.date_of_birth.year
                     users[user.username] = [langs, "%s %s"%(user.first_name,
                                                             user.last_name),
-                                            self.age(user.date_of_birth.day, user.date_of_birth.month, user.date_of_birth.year)[2]]
+                                            age(user.date_of_birth.day, user.date_of_birth.month, user.date_of_birth.year)[2]]
                     #print 456
         print request.user
         del users["%s"%request.user]
@@ -312,6 +327,8 @@ class UploadRequest(CsrfExemptMixin, LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         var = dict(request.POST)
+        if (not str(request.FILES["docfile"]).lower().endswith(".gif")) and (not str(request.FILES["docfile"]).lower().endswith(".jpg")) and (not str(request.FILES["docfile"]).lower().endswith(".jpeg")) and (not str(request.FILES["docfile"]).lower().endswith(".png")):
+            return HttpResponse("<html><head><title>File Upload</title></head><body><b>The image must be .gif, .jpg, .jpeg or .png</b></body></html>", content_type="text/html")
         userGet = User.objects.get(username=request.user)
         print userGet.email
         print var["smg"]
@@ -319,8 +336,8 @@ class UploadRequest(CsrfExemptMixin, LoginRequiredMixin, View):
         print 2
         newdoc.save()
         print request.FILES["docfile"]
-        h = "/home/rafael/meetglobal/media/imgProfiles/%s"%request.FILES['docfile']
-        h2 = "/home/rafael/meetglobal/media/imgProfiles/%s"%userGet.email
+        h = "media/imgProfiles/%s"%request.FILES['docfile']
+        h2 = "media/imgProfiles/%s"%userGet.email
         print h
         os.rename(h, h2)
         #newdoc.save()
